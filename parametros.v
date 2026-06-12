72fb11664386257d158ba84ab378ef90cda02d16
Levi <levisenandrade@gmail.com>
2026-06-12 14:30:49 -0300

    cria arquivo parameters.v

---
+++ parametros.v
@@ -0,0 +1,22 @@
+`ifndef PARAMETROS_V
+`define PARAMETROS_V
+
+// ====================
+// CORES
+// ====================
+
+`define VERMELHO 2'b00  // Navio atingido
+`define AZUL     2'b01  // Água intacta
+`define AMARELO  2'b10  // Água atingida
+`define BRANCO   2'b11  // Navio intacto
+
+// ====================
+// TIPOS DE NAVIO
+// ====================
+
+`define PORTA_AVIOES 2'b00
+`define FRAGATA      2'b01
+`define CORVETA      2'b10
+`define SUBMARINO    2'b11
+
+`endif
