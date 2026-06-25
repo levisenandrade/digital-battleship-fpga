0796f70955783eceb555b45640b5b3d1aabb1775
Carlos Daniel <168373952+Zarconiel@users.noreply.github.com>
2026-06-25 15:19:09 -0300

    Add pulse detector module for button input
    Implement pulse detector module that triggers on button press.

---
+++ detector_borda.v
@@ -0,0 +1,21 @@
+\\Este módulo gera apenas um pulso quando o botão é pressionado.
+module detector_borda(
+    input CLK,
+    input RST,
+    input Entrada,
+    output Saida
+);
+
+reg atraso;
+
+always @(posedge CLK or posedge RST)
+begin
+    if(RST)
+        atraso <= 1'b0;
+    else
+        atraso <= Entrada;
+end
+
+assign Saida = Entrada & ~atraso;
+
+endmodule
