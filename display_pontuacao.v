f8f348ccd142ea7307a55f595a1552058fac3a34
Levi <levisenandrade@gmail.com>
2026-07-01 16:07:17 -0300

    criacao do "display_pontuacao"
    funciona ate de 0 a 63 a pontuacao no display de 7 segmetnps

---
+++ display_pontuacao.v
@@ -0,0 +1,23 @@
+module display_pontuacao(
+    input [5:0] pontuacao,
+    output [6:0] HEX1,
+    output [6:0] HEX0
+);
+
+wire [3:0] dezena;
+wire [3:0] unidade;
+
+assign dezena = pontuacao / 10;
+assign unidade = pontuacao % 10;
+
+display_numero D0(
+    .numero(unidade),
+    .seg(HEX0)
+);
+
+display_numero D1(
+    .numero(dezena),
+    .seg(HEX1)
+);
+
+endmodule
