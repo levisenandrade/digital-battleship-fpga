eefb9c554fdd90e13d9d5bf1168c5856f061b828
Levi <levisenandrade@gmail.com>
2026-07-03 15:25:25 -0300

    reset assincrono adicionado
    reset assincrono adicionado + correção dos parâmetros já feita + remoção do comentário/sugsetão no Done/NextDone, pra ter o delay de um ciclo de clock pra não ocorrer da MEF2 barrar a escrita na memória

--- resetDoAmarelo.v
+++ resetDoAmarelo.v
@@ -1,37 +1,40 @@
 module resetDoAmarelo(
-    input toggle,
-    input [1:0] info, // info precisa ter 2 bits para comparar com as cores
     input clk,
+    input rst, // reset asisncrono adicionado
+    input toggle,
+    input [1:0] info,
     output reg WriteMode,
     output reg [1:0] cor,
     output reg Done
 );
 
-    // Variável interna para armazenar o próximo estado do Done
     reg NextDone;
 
-    always @(posedge clk) begin
-        Done <= NextDone; 
-
-        if(toggle) begin
-            if(info == `AMARELO) begin
-                WriteMode <= 1'b1;
-                cor       <= `AZUL;
-                NextDone  <= 1'b1; // Alimenta o registrador para o próximo ciclo
-                            // acho que não precisamos do NextDone aqui, poderia ser o Done logo, não é não? - Levi
-                
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin // ADICIONADO: bloco de reset
+            WriteMode <= 1'b0;
+            cor       <= `AZUL;
+            Done      <= 1'b0;
+            NextDone  <= 1'b0;
+        end
+        else begin
+            Done <= NextDone;
+            if(toggle) begin
+                if(info == `AMARELO) begin
+                    WriteMode <= 1'b1;
+                    cor       <= `AZUL;
+                end
+                else begin
+                    WriteMode <= 1'b0;
+                    cor       <= info;
+                end
+                NextDone <= 1'b1;
             end
             else begin
                 WriteMode <= 1'b0;
                 cor       <= info;
-                NextDone  <= 1'b1; // Se não for amarelo, também avança
-                // mesma coisa aqwui, acho que fecha o end e logo depois vai para o Done <= 1'b1;
+                NextDone  <= 1'b0;
             end
-        end
-        else begin
-            WriteMode <= 1'b0;
-            cor       <= info;
-            NextDone  <= 1'b0;
         end
     end
     
