46e9030152958b3a77d8ad10ffff6ed398011aee
Levi <levisenandrade@gmail.com>
2026-07-06 21:52:43 -0300

    display dos numeros da coordenada atualizado
    o display de 7 segmentos agora exibe de 1 a 8 ao invés de 0 a 7 no posicionamento das coordenadas

--- display_numero.v
+++ display_numero.v
@@ -1,24 +1,23 @@
 module display_numero(
-    input [3:0] numero,
+    input [3:0] numero,      // a entrada permanece sendo os valores de 0 a 7
     output reg [6:0] seg
 );
 
 // gfedcba
-// logica invertida
+// logica invretida - 0 acende, 1 apaga
 
 always @(*) begin
     case(numero)
-        4'd0: seg = 7'b1000000;
-        4'd1: seg = 7'b1111001;
-        4'd2: seg = 7'b0100100;
-        4'd3: seg = 7'b0110000;
-        4'd4: seg = 7'b0011001;
-        4'd5: seg = 7'b0010010;
-        4'd6: seg = 7'b0000010;
-        4'd7: seg = 7'b1111000;
-        4'd8: seg = 7'b0000000;
-        4'd9: seg = 7'b0010000;
-        default: seg = 7'b1111111;
+
+        4'd0: seg = 7'b1111001; // 1
+        4'd1: seg = 7'b0100100; // 2
+        4'd2: seg = 7'b0110000; // 3
+        4'd3: seg = 7'b0011001; // 4
+        4'd4: seg = 7'b0010010; // 5
+        4'd5: seg = 7'b0000010; // 6
+        4'd6: seg = 7'b1111000; // 7
+        4'd7: seg = 7'b0000000; // 8
+        default: seg = 7'b1111111; // apagado
     endcase
 end
 
