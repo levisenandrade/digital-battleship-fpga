be8c78f4d0f887e5fe5b689c764649e6ae15bf00
Levi <levisenandrade@gmail.com>
2026-07-08 10:12:34 -0300

    adiciona tipo_destruido
    adiciona um reg de tipo destruido

--- contador_destruicao.v
+++ contador_destruicao.v
@@ -6,66 +6,51 @@ module contador_destruicao(
     output wire destPA,
     output wire destFG,
     output wire destCT,
-    output wire destSB
+    output wire destSB,
+    output reg [1:0] tipo_destruido
 );
-
 reg [2:0] cnt_porta_avioes;
 reg [2:0] cnt_fragata;
 reg [2:0] cnt_corveta;
 reg [1:0] cnt_submarino;
-
+	
 // contadores
-
 always @(posedge clk or posedge rst) begin
-
     if(rst) begin
-
         cnt_porta_avioes <= 3'd5;
         cnt_fragata      <= 3'd4;
         cnt_corveta      <= 3'd3;
         cnt_submarino    <= 2'd2;
-
+        tipo_destruido   <= 2'd0;
     end
-
     else if(acerto) begin
-
+        tipo_destruido <= tipo_navio;
         case(tipo_navio)
-
             `PORTA_AVIOES: begin
                 if(cnt_porta_avioes > 3'd0)
                     cnt_porta_avioes <= cnt_porta_avioes - 3'd1;
             end
-
             `FRAGATA: begin
                 if(cnt_fragata > 3'd0)
                     cnt_fragata <= cnt_fragata - 3'd1;
             end
-
             `CORVETA: begin
                 if(cnt_corveta > 3'd0)
                     cnt_corveta <= cnt_corveta - 3'd1;
             end
-
             `SUBMARINO: begin
                 if(cnt_submarino > 2'd0)
                     cnt_submarino <= cnt_submarino - 2'd1;
             end
-
             default: begin
-                // Nenhuma ação
+				
             end
-
         endcase
-
     end
-
 end
 
-// indicam quando cada embarcação foi destruída
-	
 assign destPA = (cnt_porta_avioes == 3'd0);
 assign destFG = (cnt_fragata      == 3'd0);
 assign destCT = (cnt_corveta      == 3'd0);
 assign destSB = (cnt_submarino    == 2'd0);
-
 endmodule
